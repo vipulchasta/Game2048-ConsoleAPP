0ba0349deffdf4cfb26dba7cb986bc5610551fe4
Vipul Chasta <chasta.vipul@gmail.com>
2016-12-11 00:18:58 +0530

    Create WinCheck.h

---
+++ WinCheck.h
@@ -0,0 +1,8 @@
+class WinCheck
+{
+public:
+    static bool anyFree( int mat[][4] );      //Returns true if there is any free space
+    static bool isWon( int mat[][4] );       //Returns true if there is any 2048 element
+    static bool isAnyMove( int mat[][4] );   //Returns true if there is any move even there is no free space
+
+};
