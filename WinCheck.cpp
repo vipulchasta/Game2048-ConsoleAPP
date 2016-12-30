8b1833a761979ee2f3cafb5446a89976f42ab520
Vipul Chasta <chasta.vipul@gmail.com>
2016-12-30 21:27:53 +0530

    Update WinCheck.cpp

--- WinCheck.cpp
+++ WinCheck.cpp
@@ -11,6 +11,7 @@ bool WinCheck::anyFree( int mat[][4] )
     return false;
 }
 
+
 bool WinCheck::isWon( int mat[][4] )
 {
     for( int r_i = 0 ; r_i < 4 ; r_i++ )
@@ -26,22 +27,15 @@ bool WinCheck::isWon( int mat[][4] )
 
 bool WinCheck::isAnyMove( int mat[][4] )
 {
-    for( int r_i = 0 ; r_i < 4 ; r_i++ )
+    if( WinCheck::anyFree( mat ) )
+        return 1;
+
+    for( int r_i = 0 ; r_i < 4 ; r_i++ )                //To check if there is any move in Left and Right Move
         for( int c_i = 0 ; c_i < 3 ; c_i++ )
             if( mat[r_i][c_i] == mat[r_i][c_i+1] )
                 return true;
 
-    for( int r_i = 0 ; r_i < 4 ; r_i++ )
-        for( int c_i = 3 ; c_i > 0 ; c_i-- )
-            if( mat[r_i][c_i] == mat[r_i][c_i-1] )
-                return true;
-
-    for( int c_i = 0 ; c_i < 4 ; c_i++ )
-        for( int r_i = 3; r_i > 0 ; r_i-- )
-            if( mat[r_i][c_i] == mat[r_i-1][c_i] )
-                return true;
-
-    for( int c_i = 0 ; c_i < 4 ; c_i++ )
+    for( int c_i = 0 ; c_i < 4 ; c_i++ )                //To check if there is any move in Up or Down
         for( int r_i = 0; r_i < 3 ; r_i++ )
             if( mat[r_i][c_i] == mat[r_i+1][c_i] )
                 return true;
